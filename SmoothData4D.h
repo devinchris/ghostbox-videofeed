d65a7ff30ee4952d5a56fb4dddbdbfbdbd757e8b
Devin Rohrer <79963640+devinchris@users.noreply.github.com>
2025-11-06 10:48:36 +0100

    Add SmoothQuaternionData class header file

---
+++ SmoothData4D.h
@@ -0,0 +1,32 @@
+#ifndef SmoothData4D_h
+#define SmoothData4D_h
+#include <math.h>
+#include <ReefwingAHRS.h>
+
+class SmoothQuaternionData {
+    public:
+        SmoothQuaternionData(); // Konstruktor
+        void smoothQuaternions(Quaternion& _Quat, unsigned long currentTime);
+        void initSmoothing(float targetFrequency, float alpha=0.55f);
+
+    private:
+        unsigned long lastSmoothedTime;
+        float targetFrequency;
+        float alpha;
+        float FAST_ALPHA;
+        float DEADZONE;
+        float MOTION_THRESHOLD;
+        float _smoothedQuaternions[4];
+        float _lastQuaternions[4];
+        float qDiff[4];
+        float qBetrag;
+        bool _isInitialized;
+        void normalizeQuaternion(float q[4]);
+        /*
+         *int T_RELAX;
+         *int delta;
+         *unsigned long deltaTime;
+         */
+};
+
+#endif
